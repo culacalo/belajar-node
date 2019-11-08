e1cc9a2b942b1ea5b8245823b50bb779ba3f3b55
unknown <marselino36@gmail.com>
2019-11-09 02:43:23 +0700

    add andrre module

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const userRoute = require('@user/routes');
 const ZakyController = require('@zaky/controllers');
+const AndrewController = require('@andrew/controllers');
+const MarselinoController = require('@marselino/controllers');
+
 
 module.exports = (app) => {
   const zakyController = new ZakyController();
+  const andrewController = new AndrewController();
+  const marselinoController = new MarselinoController();
   userRoute(app);
 
   app.route('/')
@@ -12,4 +17,11 @@ module.exports = (app) => {
 
   app.route('/zaky')
     .get(zakyController.index);
+
+  app.route('/andrew')
+	  .get(andrewController.index);
+
+	app.route('/marselino') 
+		.get(marselinoController.index);
+
 };
