fab2583da134fe362917ec2648adad7c07ef2705
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2019-11-09 21:08:24 +0700

    JANDA

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const userRoute = require('@user/routes');
 const ZakyController = require('@zaky/controllers');
-
-module.exports = (app) => {
+const JandaController = require('@ichsan/controller');
+module.exports = app => {
   const zakyController = new ZakyController();
+  const jandaController = new JandaController();
+
   userRoute(app);
 
-  app.route('/')
-    .get((req, res) => {
-      res.send('Hi Apa Aceh!');
-    });
+  app.route('/').get((req, res) => {
+    res.send('Hi Apa Aceh!');
+  });
 
-  app.route('/zaky')
-    .get(zakyController.index);
+  app.route('/zaky').get(zakyController.index);
+
+  app.route('/ichsan').get(jandaController.index);
 };
