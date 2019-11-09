7e01948cc96523921a07c331cbdbba67d510b776
farhanramadhana <farhanramadhana97@gmail.com>
2019-11-09 22:01:59 +0700

    resolve conflict

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 const userRoute = require('@user/routes');
 const ZakyController = require('@zaky/controllers');
-<<<<<<< HEAD
 const AndrewController = require('@andrew/controllers');
 const MarselinoController = require('@marselino/controllers');
+const JandaController = require('@ichsan/controllers');
+const FarhanController = require('@farhan/controllers');
 
 
 module.exports = (app) => {
   const zakyController = new ZakyController();
   const andrewController = new AndrewController();
   const marselinoController = new MarselinoController();
-=======
-const JandaController = require('@ichsan/controllers');
-const FarhanController = require('@farhan/controllers');
-
-module.exports = (app) => {
-  const zakyController = new ZakyController();
   const farhanController = new FarhanController();
   const jandaController = new JandaController();
->>>>>>> master
-  userRoute(app);
 
   app.route('/').get((req, res) => {
     res.send('Hi Apa Aceh!');
@@ -26,21 +19,17 @@ module.exports = (app) => {
 
   app.route('/zaky')
     .get(zakyController.index);
-<<<<<<< HEAD
 
   app.route('/andrew')
 	  .get(andrewController.index);
 
 	app.route('/marselino') 
 		.get(marselinoController.index);
-=======
   
   app.route('/farhan')
     .get(farhanController.index);
 
   app.route('/zaky')
     .get(zakyController.index);
->>>>>>> master
-
 };
 
