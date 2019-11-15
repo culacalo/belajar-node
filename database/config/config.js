bd2b7bf0bb0538e278d299a77223c50968d852d0
muhammad.zaky.pnl@gmail.com <muhammad.zaky.pnl@gmail.com>
2019-11-15 18:25:08 +0700

    update config sequelize

--- database/config/config.js
+++ database/config/config.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
