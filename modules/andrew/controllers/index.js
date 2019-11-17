1deaad59aaf7841550d228ddeba72f8104a5baa0
muhammad.zaky.pnl@gmail.com <muhammad.zaky.pnl@gmail.com>
2019-11-17 14:54:11 +0700

    add module andrew

--- modules/andrew/controllers/index.js
+++ modules/andrew/controllers/index.js
@@ -1,12 +1,22 @@
-class AndrewController{
-	constructor(){
+const AndrewService = require('@andrew/services');
+class AndrewController {
+	constructor() {
+		this.andrewService = new AndrewService();
 		this.index = this.index.bind(this);
+		this.getById = this.getById.bind(this);
 	}
 
-	async index(req, res){
+	async index(req, res) {
 		res.send({
-			andrew:"jelek"
+			data: await this.andrewService.index()
 		});
 	}
+
+	async getById(req,res){
+		res.send({
+			data: await this.andrewService.getById(req.params.id)
+		})
+	}
 }
+
 module.exports = AndrewController;
