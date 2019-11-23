b868283b731ce280ca1cc91d7b36ec9706de66c8
aulianza <aulianza01@gmail.com>
2019-11-23 23:21:42 +0700

    add insert, update, delete and create migration at aulianza module

--- modules/aulianza/models/index.js
+++ modules/aulianza/models/index.js
@@ -1,19 +1,48 @@
 const DBService = require('@common/services/db.common.service.js');
 
 class AulianzaModel {
-    constructor() {
+    constructor(){
+        this.table = 'aulianza';
         this.dbService = new DBService();
     }
 
-    async index() {
-        const query = `SELECT * FROM aulianza`;
+    // index function 
+    async index(){
+        // intial query from database 
+        const query = `SELECT * FROM ${this.table}`;
+        // return query 
         return await this.dbService.query(query);
     }
 
-    async getById(id) {
-        const query = `SELECT * FROM aulianza WHERE id = ?`;
+    async getById(id){
+        const query = `SELECT * FROM ${this.table} WHERE id = ?`;
         return await this.dbService.query(query, id);
     }
+
+    async insert(data){
+        const query = `INSERT INTO ${this.table} SET ?`;
+        const result = await this.dbService.query(query, data);
+        return result;
+    }
+
+    async update(userId, data){
+        const query = `UPDATE ${this.table} SET ? WHERE id = ?`
+        const result = await this.dbService.query(query, [
+            data,
+            userId
+        ])
+        return result;
+    }
+
+    async delete(userId, data){
+        const query = `UPDATE ${this.table} SET ? WHERE id = ?`
+        const result = await this.dbService.query(query, [
+            data,
+            userId
+        ])
+        return result;
+    }
+
 }
 
 module.exports = AulianzaModel;
