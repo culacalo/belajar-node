e97c2e4b51983209167c1fd342e2426bd8d5f2e2
Wanda Ichsanul Isra <wandaisra@gmail.com>
2019-12-07 22:33:20 +0700

    Add pagination, filter, search & sort features

--- modules/wan/models/index.js
+++ modules/wan/models/index.js
@@ -6,9 +6,44 @@ class WanModel {
         this.dbService = new DBService();
     }
 
-    async index() {
-        const query = `SELECT * FROM culacalo_wan WHERE is_deleted=0`;
+    async index(offset = 0, limit = 10, minAge, maxAge, searchQuery, sortBy = 'id', sequence = 'DESC') {
+        let query = `SELECT * FROM ${this.table} WHERE is_deleted=0`;
+
+        if (minAge) {
+            query += ` AND age >= ${minAge}`;
+        }
+
+        if (maxAge) {
+            query += ` AND age <= ${maxAge}`;
+        }
+
+        if (searchQuery) {
+            query += ` AND name LIKE '%${searchQuery}%'`;
+        }
+
+        query += ` ORDER BY ${sortBy} ${sequence} LIMIT ${offset}, ${limit}`;
+
         return await this.dbService.query(query);
+    }
+
+    async getTotalUser(minAge, maxAge, searchQuery) {
+        let query = `SELECT COUNT(id) AS total_user FROM ${this.table} WHERE is_deleted = 0`;
+
+        if (minAge) {
+            query += ` AND age >= ${minAge}`;
+        }
+
+        if (maxAge) {
+            query += ` AND age <= ${maxAge}`;
+        }
+
+        if (searchQuery) {
+            query += ` AND name LIKE '%${searchQuery}%'`;
+        }
+
+        const result = await this.dbService.query(query);
+
+        return result[0].total_user;
     }
 
     async getUserByID(id) {
