c7a654ec4a6f2658bd3049accad3402039cc7996
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2019-12-07 23:18:53 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require("module-alias/register");
+require('module-alias/register');
 
-const express = require("express");
-const pino = require("express-pino-logger");
-const bodyParser = require("body-parser");
-const server = require("@config/server.js");
-const routes = require("./routes");
-const Sequelize = require("sequelize");
+const express = require('express');
+const pino = require('express-pino-logger');
+const bodyParser = require('body-parser');
+const server = require('@config/server.js');
+const routes = require('./routes');
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_DB,
@@ -13,17 +13,17 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: 'mysql'
   }
 );
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   })
   .catch(err => {
-    console.error("Unable to connect to t1he database:", err);
+    console.error('Unable to connect to the database:', err);
   });
 
 const app = express();
@@ -35,8 +35,8 @@ routes(app);
 
 app.use(pino);
 
-const port = process.env.SERVER_PORT || server.port;
-const host = process.env.SERVER_HOST || server.host;
+const port = process.env.PORT || server.port;
+const host = process.env.HOST || server.host;
 
 module.exports = app.listen(port, host);
 
